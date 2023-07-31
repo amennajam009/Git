6b3e7d984facf99991482696ab3242d001103482
amennajam009 <amennajam7@gmail.com>
2023-08-01 03:48:44 +0500

    add

---
+++ git-branch.js
@@ -0,0 +1,4 @@
+// In Git, branches are a fundamental concept used for organizing and managing different lines of development within a project. They allow you to work on different features, bug fixes, or experiments independently without affecting the main codebase until you're ready to merge your changes
+
+
+// git checkout -b "yourbranchName"
