25d826b4593130535995b4ba56a17334c6141873
amennajam009 <amennajam7@gmail.com>
2023-08-01 03:56:51 +0500

    add

---
+++ git-Learning/git-commit.js
@@ -0,0 +1,26 @@
+//To add the changes we make in our code we'll use the method 
+
+
+
+//step # 1
+//git status
+//this will show us the files in which we have done our work
+
+
+
+
+//step #2
+//git add .
+//this will add our code
+
+
+
+//step #3
+//git commit -m "add changes"
+//this will commit our code and pass the message
+
+
+
+//step #4
+//git push origin "your branch name"
+//this will push our code successfully!!
