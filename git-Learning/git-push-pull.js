f8e0a80addceaa6f6e534fcbf50c1f498cac9f97
amennajam009 <amennajam7@gmail.com>
2023-08-01 04:05:12 +0500

    add

---
+++ git-Learning/git-push-pull.js
@@ -0,0 +1,17 @@
+//To take a pull from any branch means you're getting the updated code of the branch which you want 
+
+
+
+//#1
+//The command used to take pull 
+// git pull origin "your branch Name"
+
+
+
+
+//To push the code 
+
+
+//#2
+//The command used to push the code
+// git push origin "your branch Name"
