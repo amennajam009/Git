38c0b93c672e81842b362f04f5766751b187e31a
amennajam009 <amennajam7@gmail.com>
2023-09-25 00:12:53 +0500

    add

--- mergedata.js
+++ mergedata.js
@@ -12,3 +12,9 @@
 //2: after Working in the new branch we we got 1 master branch and one which we just created 
 
 //3: now what if we want to merge the data of new branch into my master branch
+
+//4: simply we'll go to our github and click on (Compare and pull request) which it already showing us when we push data in our new branch
+
+//5: Then we'll Click on create pull request
+
+//6: If there's no conflit then we're ready to push it in master branch :) 
