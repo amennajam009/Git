5c5700f89ad46f4df69044d1d63488ee81f51fd1
amennajam009 <amennajam7@gmail.com>
2023-09-24 23:55:58 +0500

    add

--- mergedata.js
+++ mergedata.js
@@ -1,2 +1,8 @@
 //Today we're gonna learn about to merge the different branches to master branch 
 //We'll do it from scratch 
+
+
+
+
+
+//1: First we'll make new Branch in which we will do som work so that we can merge the data later
