177d189e4601f5061434b0122574cdc20d174093
amennajam009 <amennajam7@gmail.com>
2023-09-25 00:08:58 +0500

    add

--- mergedata.js
+++ mergedata.js
@@ -7,3 +7,6 @@
 
 //1: First we'll make new Branch in which we will do som work so that we can merge the data later
            //git checkout -b "newbranchname"
+
+
+//2: after Working in the new branch we we got 1 master branch and one which we just created           
